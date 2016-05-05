55a04038a977c097d879050fc45eead20d595276
bustosalex <a.bustos0713@gmail.com>
2016-05-05 14:15:04 -0500

    Created the core data class for the product to be added and started to save the product

---
+++ Products.swift
@@ -0,0 +1,18 @@
+//
+//  Products.swift
+//  productpal
+//
+//  Created by Alexander Bustos on 5/5/16.
+//  Copyright © 2016 University of Wisconsin Parkisde. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Products: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+
+}
